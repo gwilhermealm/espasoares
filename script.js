bd705c7b19d65e928032589f36610546493c357c
gwilhermealm <guilhermealmeidze@gmail.com>
2026-03-20 22:54:50 -0300

    quase pronto

---
+++ script.js
@@ -0,0 +1,30 @@
+function NavbarLateral() { 
+    const navbar = document.getElementById('nav-lateral');
+    navbar.classList.toggle('nav-close');
+    navbar.classList.toggle('nav-open');
+
+
+}
+function NavbarLateralclose() {
+    const navbar = document.getElementById('nav-lateral');
+    navbar.classList.add('nav-close');
+    navbar.classList.remove('nav-open');
+}
+
+function mostrarseçao(classname) {
+    const secoes = document.querySelectorAll('#secao-comodis');
+    
+
+
+    secoes.forEach(secao => {
+       secao.style.display = 'none';
+    });
+    const secaoSelecionada = document.querySelector('.' + classname);
+    if (secaoSelecionada) {
+    secaoSelecionada.style.display = 'flex';
+   
+    
+    
+    }else {        console.log('Secao nao encontrada');
+    }
+}
